3ec3f590843beb6fe8064bad69521a316433d734
JihoPark <jihopark.ai@gmail.com>
2025-02-07 16:43:54 +0900

    Time: 322 ms (89.56%), Space: 66.6 MB (16.98%) - LeetHub

--- 3073-reshape-data-melt/3073-reshape-data-melt.py
+++ 3073-reshape-data-melt/3073-reshape-data-melt.py
@@ -2,10 +2,10 @@ import pandas as pd
 
 def meltTable(report: pd.DataFrame) -> pd.DataFrame:
     
-    # return report.melt(id_vars='product', 
-    #     value_vars=['quarter_1', 'quarter_2', 'quarter_3', 'quarter_4'],
-    #     var_name='quarter', 
-    #     value_name='sales'
-    #     )
+    return report.melt(id_vars='product', 
+        value_vars=['quarter_1', 'quarter_2', 'quarter_3', 'quarter_4'],
+        var_name='quarter', 
+        value_name='sales'
+        )
 
-    return pd.melt(report, id_vars=['product'], var_name='quarter', value_name='sales')
+    # return pd.melt(report, id_vars=['product'], var_name='quarter', value_name='sales')
