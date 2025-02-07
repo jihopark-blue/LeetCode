37cd98338a3c35cf177f52828f86a8c3d3d33167
JihoPark <jihopark.ai@gmail.com>
2025-02-07 15:11:54 +0900

    Time: 78 ms (9.44%), Space: 20.5 MB (6.52%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -1,29 +1,10 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        
-        # {element: [count, [index]]}
-        cnt = {n:[0, []] for n in nums}
+        k = 2
 
-        # 삭제해야 하는 idx
-        del_idx = []
-        
-        for i in range(len(nums)):
+        for i in range(2, len(nums)):
+            if nums[i] != nums[k - 2]:
+                nums[k] = nums[i]
+                k += 1 
 
-            idx = i
-            element = nums[i]
-
-            cnt[element][0] += 1
-            cnt[element][1].append(idx)
-            
-            # 등장 횟수 2 초과하는 idx 기록
-            if len(cnt[element][1])>2:
-                del_idx.append(idx)
-
-        # idx 뒤 원소부터 삭제
-        del_idx.sort(reverse=True)
-
-        for j in del_idx:
-            nums.pop(j)
-
-        k = len(nums)
         return k
