831279226b82be8516f0c374f83b3e872a09ceec
JihoPark <jihopark.ai@gmail.com>
2025-02-07 16:14:50 +0900

    Time: 543 ms (32.69%), Space: 65.9 MB (45.87%) - LeetHub

--- 3070-fill-missing-data/3070-fill-missing-data.py
+++ 3070-fill-missing-data/3070-fill-missing-data.py
@@ -3,4 +3,6 @@ import pandas as pd
 def fillMissingValues(products: pd.DataFrame) -> pd.DataFrame:
     
     products.fillna(value={'quantity': 0}, inplace=True)
+    # products['quantity'] = products['quantity'].fillna(0)
+
     return products
